f5b557f2625df72ee64595ba6fa3102356fb0e91
Carwin Young <carwinyoung@gmail.com>
2013-03-07 00:03:19 -0600

    Created the needed file/directory for drush aliases to work.

---
+++ drush/aliases/dsgf.aliases.drushrc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Drush aliases file.
+ */
+
+/**
+ * These are default configurations that can be overwritten.
+ */
+
+$aliases['live'] = array(
+  'root' => '/var/www/vhosts/drupalsgf.org/docroot',
+  'remote-host' => 'drupalsgf.org',
+);
+
